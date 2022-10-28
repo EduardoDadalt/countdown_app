c922386fb6ac05de062267d2f95a156ed3736f6d
Eduardo Dadalt Andres <eduardodadalt1@gmail.com>
2022-10-27 23:31:51 -0300

    :arrow_up: 1.1.0+2
    Add remove button to countdown

--- lib/homepage/view/countdown_view.dart
+++ lib/homepage/view/countdown_view.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
+import 'package:countdown_app/homepage/controller/countdown_controller.dart';
 import 'package:countdown_app/homepage/domain/entity/countdown.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CountdownView extends StatefulWidget {
   const CountdownView({
@@ -19,17 +20,25 @@ class CountdownView extends StatefulWidget {
 class _CountdownViewState extends State<CountdownView> {
   late Duration duration = calcDuration();
 
+  Timer? timer;
+
   Duration calcDuration() => widget.countdown.date.difference(DateTime.now());
 
   @override
   void initState() {
-    Timer.periodic(const Duration(seconds: 1), (_) {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) {
       setState(() {
         duration = calcDuration();
       });
     });
 
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 
   String formatDuration(Duration duration) {
@@ -50,8 +59,21 @@ class _CountdownViewState extends State<CountdownView> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(widget.countdown.title,
-                style: Theme.of(context).textTheme.headline6),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.countdown.title,
+                    style: Theme.of(context).textTheme.headline6),
+                IconButton(
+                  onPressed: () => {
+                    Provider.of<CountdownController>(context, listen: false)
+                        .removeCountdown(widget.countdown)
+                  },
+                  icon: const Icon(Icons.delete),
+                  iconSize: 20,
+                )
+              ],
+            ),
             Text(
               formatDuration(duration),
               style: Theme.of(context).textTheme.headline4,
